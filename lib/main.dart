7005b6c3b9b8b61e90f960b70a2a287e47e838c6
Michael Christenson II <m3talsmith@gmail.com>
2024-11-05 10:49:53 -0500

    Set decent default window size for desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kubectl_dashboard/app.dart';
@@ -6,6 +9,12 @@ import 'package:kubectl_dashboard/app/config/providers.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+    final view = WidgetsBinding.instance.platformDispatcher.views.first;
+    await DesktopWindow.setWindowSize(
+      Size(view.physicalSize.width / 3, view.physicalSize.height / 3),
+    );
+  }
   final loadedConfigs = await loadConfigs();
   runApp(
     ProviderScope(
