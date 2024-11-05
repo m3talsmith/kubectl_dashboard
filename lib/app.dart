248f06940f524253fd0429e13d45cac32174784c
Michael Christenson II <m3talsmith@gmail.com>
2024-11-05 10:28:38 -0500

    Refactor out app drawer

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kubectl_dashboard/app/config.dart';
-import 'package:kubectl_dashboard/app/config/add_config.dart';
-import 'package:kubectl_dashboard/app/config/config_tile_item.dart';
+import 'package:kubectl_dashboard/app/app_drawer.dart';
 
 import 'app/config/providers.dart';
 
@@ -30,51 +29,7 @@ class App extends ConsumerWidget {
             title: const Text('Kubectl Dashboard'),
           ),
           body: const Text('home'),
-          endDrawer: Drawer(
-            child: ListView(
-              children: [
-                if (configs != null)
-                  ...configs.map((e) {
-                    final index = configs.indexOf(e);
-
-                    onTap() {
-                      ref.watch(currentConfigIndexProvider.notifier).state =
-                          index;
-                      Navigator.of(context).pop();
-                    }
-
-                    return ConfigListTile(
-                      config: e,
-                      selected: currentConfig,
-                      onTap: onTap,
-                    );
-                  }),
-                const Divider(),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton.icon(
-                    onPressed: () async {
-                      final Config? config =
-                          await Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const AddConfigForm(),
-                      ));
-                      if (config != null) {
-                        final c = (configs == null) ? [config] : configs
-                          ..add(config);
-                        final index = c.length - 1;
-                        ref.watch(currentConfigIndexProvider.notifier).state =
-                            index;
-                        ref.watch(configsProvider.notifier).state = c;
-                        ref.invalidate(configsProvider);
-                      }
-                    },
-                    icon: const Icon(Icons.add),
-                    label: const Text('Add Cluster'),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          endDrawer: const AppDrawer(),
         );
       }),
     );
---
+++ lib/app/app_drawer.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'config.dart';
+import 'config/add_config.dart';
+import 'config/config_tile_item.dart';
+import 'config/providers.dart';
+
+class AppDrawer extends ConsumerWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final configs = ref.watch(configsProvider);
+    ref.watch(configsProvider.notifier).addListener(saveConfigs);
+
+    final currentConfigIndex = ref.watch(currentConfigIndexProvider);
+    final Config? currentConfig = configs?[currentConfigIndex];
+
+    return Drawer(
+      child: ListView(
+        children: [
+          if (configs != null)
+            ...configs.map((e) {
+              final index = configs.indexOf(e);
+
+              onTap() {
+                ref.watch(currentConfigIndexProvider.notifier).state =
+                    index;
+                Navigator.of(context).pop();
+              }
+
+              return ConfigListTile(
+                config: e,
+                selected: currentConfig,
+                onTap: onTap,
+              );
+            }),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+              onPressed: () async {
+                final Config? config =
+                await Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const AddConfigForm(),
+                ));
+                if (config != null) {
+                  final c = (configs == null) ? [config] : configs
+                    ..add(config);
+                  final index = c.length - 1;
+                  ref.watch(currentConfigIndexProvider.notifier).state =
+                      index;
+                  ref.watch(configsProvider.notifier).state = c;
+                  ref.invalidate(configsProvider);
+                }
+              },
+              icon: const Icon(Icons.add),
+              label: const Text('Add Cluster'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
